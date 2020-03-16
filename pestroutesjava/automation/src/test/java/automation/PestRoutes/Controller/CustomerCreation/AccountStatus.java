781e1b4a9fe03fb98172cc9b19e812a0377a9e0a
aditya-morugadi <61211838+aditya-morugadi@users.noreply.github.com>
2020-03-16 16:40:16 -0500

    Removed syso and updated cancellation methods with ID

--- pestroutesjava/automation/src/test/java/automation/PestRoutes/Controller/CustomerCreation/AccountStatus.java
+++ pestroutesjava/automation/src/test/java/automation/PestRoutes/Controller/CustomerCreation/AccountStatus.java
@@ -7,14 +7,13 @@ import org.testng.annotations.Test;
 import com.aventstack.extentreports.ExtentTest;
 import automation.PestRoutes.PageObject.Header;
 import automation.PestRoutes.PageObject.CreateCustomer.CreateCustomerDIalog;
-import automation.PestRoutes.PageObject.CreateCustomer.StatusChange;
 import automation.PestRoutes.PageObject.CustomerOverview.CustomerViewDialog_Header;
 import automation.PestRoutes.PageObject.CustomerOverview.CustomerViewDialog_OverviewTab;
+import automation.PestRoutes.PageObject.CustomerOverview.CustomerViewDialog_Admin;
 import automation.PestRoutes.Utilities.AssertException;
 import automation.PestRoutes.Utilities.BaseClass;
 import automation.PestRoutes.Utilities.Reporter;
 import automation.PestRoutes.Utilities.Utilities;
-
 
 public class AccountStatus extends BaseClass {
 	static ExtentTest test;
@@ -22,12 +21,12 @@ public class AccountStatus extends BaseClass {
 	CustomerViewDialog_Header dialog = new CustomerViewDialog_Header();
 	CustomerViewDialog_OverviewTab overview = new CustomerViewDialog_OverviewTab();
 	Header header = new Header();
-	StatusChange statusChange = new StatusChange();
+	CustomerViewDialog_Admin statusChange = new CustomerViewDialog_Admin();
 	List list = new ArrayList<String>();
 
 	@Test
 	public void validateAccountStatus() throws Exception {
-		//createCustomer();
+		createCustomer();
 		validateActiveStatus();
 		validateFrozenStatus();
 		AssertException.asserFailure(list);
@@ -63,27 +62,24 @@ public class AccountStatus extends BaseClass {
 		dialog.NavigateTo(dialog.adminTabInDialog);
 		statusChange.changeAccountStatus_Active();
 		statusChange.getAccountStatus();
+		String expectedStatus = "status: [Frozen] was changed to [Active]";
 		String actualStatus = statusChange.getAccountStatus().substring(16, 57);
-		System.out.println(statusChange.getAccountStatus().substring(16, 57));
-		System.out.println(statusChange.PDTDate());
 		list = AssertException.result("status: [Frozen] was changed to [Active]", actualStatus, "Frozen Status");
-		Reporter.status("Activate Status", "status: [Frozen] was changed to [Active]", actualStatus, "Frozen Status");
-
+		Reporter.status("Activate Status", expectedStatus, actualStatus, "Frozen Status");
+		list.add(AssertException.result(expectedStatus, actualStatus, "Validate Active Status"));
 	}
 
 	// Change the account to Frozen and assert
 	private void validateFrozenStatus() throws IOException, Exception {
 		header.Search_A_Customer(getData("userID", generalData));
 		dialog.NavigateTo(dialog.adminTabInDialog);
-		statusChange.changeAccountStatus_Frozen(statusChange.medical, statusChange.cancelServiceProps);
+		statusChange.changeAccountStatus_Frozen(statusChange.cancelServiceProps);
 		statusChange.getAccountStatus();
+		String expectedStatus = "status: [Active] was changed to [Frozen]";
 		String actualStatus = statusChange.getAccountStatus().substring(16, 57);
-		System.out.println(statusChange.getAccountStatus().substring(16, 57));
-		System.out.println(statusChange.PDTDate());
-		list = AssertException.result("status: [Active] was changed to [Frozen]", actualStatus, "Frozen Status");
-		Reporter.status("Activate Status", "status: [Active] was changed to [Frozen]", actualStatus, "Frozen Status");
-
+		list = AssertException.result(expectedStatus, actualStatus, "Frozen Status");
+		Reporter.status("Activate Status", expectedStatus, actualStatus, "Frozen Status");
+		list.add(AssertException.result(expectedStatus, actualStatus, "Validate Frozen Status"));
 	}
-	
 
 }
---
+++ pestroutesjava/automation/src/test/java/automation/PestRoutes/PageObject/CustomerOverview/CustomerViewDialog_Admin.java
@@ -0,0 +1,104 @@
+package automation.PestRoutes.PageObject.CustomerOverview;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+import automation.PestRoutes.Utilities.FindElement;
+import automation.PestRoutes.Utilities.FindElement.InputType;
+import automation.PestRoutes.Utilities.Utilities;
+import automation.PestRoutes.Utilities.Driver.GetWebDriver;
+import automation.PestRoutes.Utilities.Utilities.ElementType;
+
+public class CustomerViewDialog_Admin {
+
+	static WebDriver driver = GetWebDriver.getInstance();;
+	public String accountStatusChange = "//div[@id='completeButton']/span";
+
+	// Cancel Category
+	public String clickCancelCatogory = "//select[@id='cancelCategory']";
+
+	// Cancel Customer Options
+	public String clickCancelCustomerOptions = "//select[@id='cancelCustomerOptions']";
+	public String cancelServiceProps = "Cancel Responsible Service Subscriptions";
+	public String reassignToNew = "Reassign Service Properties To New Billing Account";
+	public String reassignToSelf = "Reassign Service Properties to Themselves";
+	public String cancelNotes = "//textarea[@id='cancelNotes']";
+	private String clickFreeze = "//span[text()='Freeze Account']";
+
+	public void changeAccountStatus_Frozen(String cancelCustomerOption) {
+
+		Utilities.waitUntileElementIsVisible(accountStatusChange);
+		Utilities.clickElement(accountStatusChange, ElementType.XPath);
+		Utilities.clickElement(clickCancelCatogory, ElementType.XPath);
+		clickCancellationCategory();
+		cancellationCategory();
+		selectCancelCustomerOptions(cancelCustomerOption);
+		cancellationNotes();
+		clickFreezeAccount();
+	}
+
+	public void changeAccountStatus_Active() {
+		Utilities.waitUntileElementIsVisible(accountStatusChange);
+		Utilities.clickElement(accountStatusChange, ElementType.XPath);
+	}
+
+	public String getAccountStatus() {
+		Utilities.scrollToElement("//span[text() = '" + Utilities.currentDate("M/dd/yy h:mm aa").toLowerCase()
+				+ " CDT']/parent::div/parent::li//h4/parent::div[1]");
+		String text = driver
+				.findElement(By.xpath("//span[text() = '" + Utilities.currentDate("M/dd/yy h:mm aa").toLowerCase()
+						+ " CDT']/parent::div/parent::li//h4/parent::div[1]"))
+				.getText();
+		return text;
+	}
+
+	public void clickCancellationCategory() {
+		Utilities.clickElement(clickCancelCatogory, ElementType.XPath);
+	}
+
+	public void cancellationCategory() {
+		selectValueFromDropDownByIndex("//select[@id='cancelCategory']", 2);
+	}
+
+	public static void selectValueFromDropDownByIndex(String needXpath, Integer needIndex) {
+		Select dropdown = new Select(driver.findElement(By.xpath(needXpath)));
+		dropdown.selectByIndex(needIndex);
+	}
+
+	public void selectCancelCustomerOptions(String cancelCustomerOption) {
+		Utilities.waitUntileElementIsVisible(
+				"//select[@id='cancelCustomerOptions']/option[text() = '" + cancelCustomerOption + "']");
+		Utilities.clickElement("//select[@id='cancelCustomerOptions']/option[text() = '" + cancelCustomerOption + "']",
+				ElementType.XPath);
+	}
+
+	public void cancellationNotes() {
+		Utilities.clickElement(cancelNotes, ElementType.XPath);
+		FindElement.elementByAttribute(cancelNotes, InputType.XPath).sendKeys("Cancelling the customer");
+	}
+
+	public void clickFreezeAccount() {
+		Utilities.clickElement(clickFreeze, ElementType.XPath);
+	}
+
+	public String getActualDate() {
+		Utilities.scrollToElement("//span[text() = '" + PDTDate() + " CDT']/parent::div/parent::li//h4/parent::div[1]");
+		String text = driver
+				.findElement(
+						By.xpath("//span[text() = '" + PDTDate() + " CDT']/parent::div/parent::li//h4/parent::div[1]"))
+				.getText();
+		return text;
+	}
+
+	public String PDTDate() {
+		Date date = new Date();
+		DateFormat formatter = new SimpleDateFormat("yyyy-mm-dd HH:mm:ss");
+		formatter.setTimeZone(TimeZone.getTimeZone("PST"));
+		String strDate = formatter.format(date).toLowerCase();
+		return strDate;
+	}
+}
