91a9f48d20344a7ee1c2ec5c53369bd7daeba463
aditya-morugadi <61211838+aditya-morugadi@users.noreply.github.com>
2020-06-17 21:12:01 -0500

    Reminder Update

--- pestroutesjava/automation/src/test/java/automation/PestRoutes/Controller/Admin/Preferences/OfficeSettings/TriggerRules/Reminders/TriggerDaysBefore_Reminder.java
+++ pestroutesjava/automation/src/test/java/automation/PestRoutes/Controller/Admin/Preferences/OfficeSettings/TriggerRules/Reminders/TriggerDaysBefore_Reminder.java
@@ -1,5 +1,33 @@
 package automation.PestRoutes.Controller.Admin.Preferences.OfficeSettings.TriggerRules.Reminders;
 
-public class TriggerDaysBefore_Reminder {
+import org.testng.annotations.Test;
+import automation.PestRoutes.Utilities.BaseClass;
 
+public class TriggerDaysBefore_Reminder extends BaseClass {
+
+	CreateTrigger_Reminder createReminder = new CreateTrigger_Reminder();
+
+	private String description_TriggerBeforeDays = "TriggerBeforeDays_Reminder";
+
+	@Test
+	public void createReminderRule() throws Exception {
+
+		// Create trigger
+		createTriggerDaysBefore_Reminder(description_TriggerBeforeDays);
+
+		// Create Actions
+		Reminder_createAllActions();
+	}
+
+	// Create Trigger
+	public void createTriggerDaysBefore_Reminder(String description) throws Exception {
+		createReminder.createReminderRule();
+	}
+	
+	//Create All Actions
+	public void Reminder_createAllActions() {
+		createReminder.emailAction_Reminder();
+		createReminder.SMSAction_Reminder();
+		createReminder.voiceAction_Reminder();
+	}
 }
