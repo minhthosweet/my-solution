399a62e3de485ad305c23453fd4ad43485c22591
aditya-morugadi <61211838+aditya-morugadi@users.noreply.github.com>
2020-06-18 15:27:17 -0500

    Trigger Days Before Update

--- pestroutesjava/automation/src/test/java/automation/PestRoutes/Controller/Admin/Preferences/OfficeSettings/TriggerRules/Reminders/TriggerDaysBefore_Reminder.java
+++ pestroutesjava/automation/src/test/java/automation/PestRoutes/Controller/Admin/Preferences/OfficeSettings/TriggerRules/Reminders/TriggerDaysBefore_Reminder.java
@@ -1,33 +1,41 @@
 package automation.PestRoutes.Controller.Admin.Preferences.OfficeSettings.TriggerRules.Reminders;
 
+import automation.PestRoutes.Utilities.Utilities;
 import org.testng.annotations.Test;
 import automation.PestRoutes.Utilities.BaseClass;
 
 public class TriggerDaysBefore_Reminder extends BaseClass {
 
-	CreateTrigger_Reminder createReminder = new CreateTrigger_Reminder();
+    CreateTrigger_Reminder createReminder = new CreateTrigger_Reminder();
 
-	private String description_TriggerBeforeDays = "TriggerBeforeDays_Reminder";
+    private String description_TriggerBeforeDays = "TriggerBeforeDays_Reminder";
 
-	@Test
-	public void createReminderRule() throws Exception {
+    @Test
+    public void createReminderRule() throws Exception {
 
-		// Create trigger
-		createTriggerDaysBefore_Reminder(description_TriggerBeforeDays);
+        // Create trigger
+        createTriggerDaysBefore_Reminder(description_TriggerBeforeDays);
 
-		// Create Actions
-		Reminder_createAllActions();
-	}
+        // Create Actions
+        Reminder_createAllActions();
 
-	// Create Trigger
-	public void createTriggerDaysBefore_Reminder(String description) throws Exception {
-		createReminder.createReminderRule();
-	}
-	
-	//Create All Actions
-	public void Reminder_createAllActions() {
-		createReminder.emailAction_Reminder();
-		createReminder.SMSAction_Reminder();
-		createReminder.voiceAction_Reminder();
-	}
+        // hit trigger
+        hitTriggerQueue();
+    }
+
+    // Create Trigger
+    public void createTriggerDaysBefore_Reminder(String description) throws Exception {
+        createReminder.createTrigger_Reminder(description_TriggerBeforeDays);
+    }
+
+    //Create All Actions
+    public void Reminder_createAllActions() {
+        createReminder.emailAction_Reminder();
+        createReminder.SMSAction_Reminder();
+        createReminder.voiceAction_Reminder();
+    }
+
+    public void hitTriggerQueue() {
+        Utilities.navigateToUrl("https://adityam.pestroutes.com/resources/scripts/triggerReminders.php");
+    }
 }
