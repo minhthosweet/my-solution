7aea41ecc7ddfa43cec6c2075c214a4d32d93090
aditya-morugadi <61211838+aditya-morugadi@users.noreply.github.com>
2020-09-14 22:11:38 -0500

    On Initial Billing Date If date starts with 0, current date delimits the value '0'

--- pestroutesjava/automation/src/test/java/automation/PestRoutes/TestRunner/Smoke.java
+++ pestroutesjava/automation/src/test/java/automation/PestRoutes/TestRunner/Smoke.java
@@ -2,9 +2,15 @@ package automation.PestRoutes.TestRunner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
-@CucumberOptions(features = { "src/test/resources/features" }, plugin = { "pretty" }, glue = {
-		"automation.PestRoutes" }, tags = "@Smoke")
+@CucumberOptions(features = {"src/test/resources/features"}, plugin = {"json:target/cucumber.json", "html:target/site/cucumber-pretty", "pretty"}, glue = {
+        "automation.PestRoutes"}, tags = "@OnInitialBillingDate")
 public class Smoke extends AbstractTestNGCucumberTests {
-	
+
+    @Override
+    @DataProvider
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
