25724538dac51d00860b2bb8f30dff4c27aa1112
aditya-morugadi <61211838+aditya-morugadi@users.noreply.github.com>
2021-06-08 14:32:28 -0500

    Changes - Added test case to validate customer search by Email

--- pestroutesjava/automation/src/test/java/automation/PestRoutes/PageObject/Search/SearchBox.java
+++ pestroutesjava/automation/src/test/java/automation/PestRoutes/PageObject/Search/SearchBox.java
@@ -4,11 +4,14 @@ import automation.PestRoutes.Utilities.Utilities;
 
 public class SearchBox
 {
+
+    //Author : Adi
     public String autoCompleteSearch(String customerDetail){
         Utilities.waitUntileElementIsVisible("//li[@role='presentation']//span[text()='"+customerDetail+"']");
         return Utilities.getElementTextValue("//li[@role='presentation']//span[text()='"+customerDetail+"']", Utilities.ElementType.XPath);
     }
 
+    //Author : Adi
     public String containsInAutoCompleteSearch(String customerDetail){
         Utilities.waitUntileElementIsVisible("//li[@role='presentation']//span[contains(text(),'"+customerDetail+"')]");
         return Utilities.getElementTextValue("//li[@role='presentation']//span[contains(text(),'"+customerDetail+"')]", Utilities.ElementType.XPath);
